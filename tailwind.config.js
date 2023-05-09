b0b6d64e62fa7e44dece7ee3509d336ae4e8753f
Alejandro LR <alejandro.lr.developer@hotmail.com>
2023-05-08 22:19:23 -0500

    Add: style guide and Remove: Boilerplate

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          main: "hsl(218, 28%, 13%)",
+          footer: "hsl(216, 53%, 9%)",
+          intro: "hsl(217, 28%, 15%)",
+          testimonials: "hsl(219, 30%, 18%)",
+        },
+        accent: {
+          cyan: "hsl(176, 68%, 64%)",
+          blue: "hsl(198, 60%, 50%)",
+          error: "hsl(0, 100%, 63%)",
+        },
       },
     },
   },
   plugins: [],
-}
+};

a44bc00447a1327ebe5115001d3c899b4c547977
Alejandro LR <alejandro.lr.developer@hotmail.com>
2023-05-09 12:29:41 -0500

    Add: Home UI and layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
+import Image from 'next/image';
+
 const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello</h1>
-    </main>
+    <section className='flex flex-1 flex-col items-center gap-4'>
+      <Image src='/illustration-intro.png' alt='illustration' width={340} height={340} draggable={false} />
+      <div className='flex flex-col items-center justify-center gap-4'>
+        <h1 className='max-w-sm text-center text-3xl font-medium leading-snug md:max-w-lg'>
+          All your files in one secure location, accessible anywhere.
+        </h1>
+        <p className='max-w-xs text-center md:max-w-lg'>
+          Fylo stores all your most important files in one secure location. Access them wherever you need, share and
+          collaborate with friends family, and co-workers.
+        </p>
+        <button className='mt-4 w-full rounded-full bg-cyan-400 p-4 font-bold md:w-2/4'>Get Started</button>
+      </div>
+    </section>
   );
 };
 
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Navigation = () => {
+  return (
+    <header className='m-[24px] flex flex-row items-center justify-between'>
+      <Link href='/'>
+        <Image src='/logo.svg' alt='logo' width={100} height={100} draggable={false} />
+      </Link>
+      <ul className='flex flex-row items-center gap-6'>
+        <li className='cursor-pointer hover:underline'>Features</li>
+        <li className='cursor-pointer hover:underline'>Team</li>
+        <li className='cursor-pointer hover:underline'>Sign In</li>
+      </ul>
+    </header>
+  );
+};
+
+export default Navigation;

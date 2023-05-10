ccca87ffe2094d634c15ade472662bbe89c43a48
Alejandro LR <alejandro.lr.developer@hotmail.com>
2023-05-10 17:26:21 -0500

    Add: Tutorial component media queries

--- src/components/Tutorial.tsx
+++ src/components/Tutorial.tsx
@@ -2,9 +2,9 @@ import Image from 'next/image';
 
 const Tutorial: React.FC = () => {
   return (
-    <section className='flex flex-col items-center justify-center gap-8 md:mx-20'>
+    <section className='flex flex-col items-center justify-center gap-8 md:mx-20 md:flex-row md:gap-20'>
       <Image src='/illustration-stay-productive.png' alt='cov' height={400} width={400} />
-      <div className='flex max-w-sm flex-col items-center justify-center gap-6'>
+      <div className='flex max-w-sm flex-col items-center justify-center gap-6 md:max-w-md'>
         <h1 className='text-3xl font-medium'>Stay productive, wherever you are</h1>
         <p>
           Never let location be an issue when accessing your files. Fylo has you covered for all of your file storage
@@ -14,7 +14,9 @@ const Tutorial: React.FC = () => {
           Securely share files and folders with friends, family and colleagues for live collaboration. No email
           attachments required.
         </p>
-        <button className='mr-auto border-b border-cyan-400 text-cyan-400'>See how Fylo works</button>
+        <button className='mr-auto border-b border-cyan-400 text-cyan-400 hover:border-white hover:text-white'>
+          See how Fylo works
+        </button>
       </div>
     </section>
   );
